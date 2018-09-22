3ad55250ccf2e69aab7b93226e9d11649808482f
Dmitry Grigorev <gridmi007@gmail.com>
2018-09-22 12:03:19 +0300

    based packages and start app

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const outputDirectory = "dist";
+
+function srcPath(subdir) {
+    return path.join(__dirname, "src", subdir);
+}
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        defaultapp: ['./src/index.js']
+    },
+    output: {
+        path: path.join(__dirname, outputDirectory),
+        filename: '[name].js'
+    },
+    devtool: "source-map",
+  	plugins: [
+    	new CleanWebpackPlugin([outputDirectory]),
+    	new HtmlWebpackPlugin({
+      		title: "My App",
+      		hash: true,
+      		template: "stub/index.html"
+    	})
+  	]
+}
